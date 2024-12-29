8fe18c7b059c8691e3e9001d739cf4ce72b1d21b
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2024-12-29 19:39:55 +0500

    node.js 3

--- app.js
+++ app.js
@@ -5,4 +5,21 @@
 // console.log(os.freemem()); // opirativkadagi bo'sh joy
 // console.log(os.totalmem()); // jami hotira
 
-import path from "node:path";
+// console.log(__dirname);
+// console.log(__filename);
+
+import path from "path";
+// console.log(path.basename(`${process.cwd()}/app.js`));
+
+const pathName = path.join(process.cwd(), "apps", "app1.js");
+// console.log(path.basename(pathName));
+// console.log(path.dirname(pathName));
+// console.log(path.extname(pathName));
+// console.log(
+//   path.format({
+//     ext: ".js",
+//     name: "app",
+//     dir: process.cwd(),
+//   })
+// );
+console.log(path.parse(pathName));
