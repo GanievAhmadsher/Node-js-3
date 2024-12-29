42f1f7832d2c6a3f6e35e5a478a3e2a54c3a7439
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2024-12-29 17:44:48 +0500

    node

---
+++ app.js
@@ -0,0 +1,5 @@
+import os from "node:os";
+// console.log(os.uptime()); // yoniq turgan vaqti (screen time)
+// console.log(os.arch()); // raziryad 64
+// console.log(os.cpus()); // yadro'lar soni
+console.log(os);
