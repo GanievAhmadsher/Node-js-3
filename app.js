9b9ada12a200d6347ca17c3eb2f935c4517fadd2
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2024-12-29 18:30:24 +0500

    node.js 3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-import os from "node:os";
+// import os from "node:os";
 // console.log(os.uptime()); // yoniq turgan vaqti (screen time)
 // console.log(os.arch()); // raziryad 64
 // console.log(os.cpus()); // yadro'lar soni
-console.log(os);
+// console.log(os.freemem()); // opirativkadagi bo'sh joy
+// console.log(os.totalmem()); // jami hotira
+
+import path from "node:path";
